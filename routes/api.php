4cdeefa9f7e61eb696495deafddaf87b9fb9140d
Paul A.S. Dev <astosalaspaul@gmail.com>
2025-09-09 21:31:28 -0500

    Definiendo las rutas basicas de la api, Las rutas principales son CRUD de vehiculos, propietarios, modelos y marcas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,41 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Models\Marca;
+use App\Models\Modelo;
+use App\Models\Propietario;
+use App\Models\Vehiculo;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix("/v1")->group(function (){
+    Route::prefix("/vehiculo")->controller(Vehiculo::class)->group(function(){
+        Route::get("/{vehiculo}", "findById");
+        Route::get("/", "getAll");
+        Route::post("/", "create");
+        Route::put("/{vehiculo}", "update");
+        Route::delete("/{vehiculo}", "delete");
+    });
+
+    Route::prefix("/propietario")->controller(Propietario::class)->group(function(){
+        Route::get("/{propietario}", "findById");
+        Route::get("/", "getAll");
+        Route::post("/", "create");
+        Route::put("/{propietario}", "update");
+        Route::delete("/{propietario}", "delete");
+    });
+
+    Route::prefix("/marca")->controller(Marca::class)->group(function(){
+        Route::get("/{marca}", "findById");
+        Route::get("/", "getAll");
+        Route::post("/", "create");
+        Route::put("/{marca}", "update");
+        Route::delete("/{marca}", "delete");
+    });
+
+        Route::prefix("/modelo")->controller(Modelo::class)->group(function(){
+        Route::get("/{modelo}", "findById");
+        Route::get("/", "getAll");
+        Route::post("/", "create");
+        Route::put("/{modelo}", "update");
+        Route::delete("/{modelo}", "delete");
+    });
+});
